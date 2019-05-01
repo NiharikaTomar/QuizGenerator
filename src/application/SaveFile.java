9a9d5446c5ba6be1296f98088c6afee764aadb82
elizavetastepanova <estepanova@wisc.edu>
2019-05-01 17:07:26 -0500

    modified SaveFile

--- src/application/SaveFile.java
+++ src/application/SaveFile.java
@@ -1,5 +1,19 @@
 package application;
 
-public class SaveFile {
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.json.simple.parser.ParseException;
 
+public class SaveFile {
+  public SaveFile() throws FileNotFoundException, IOException, ParseException {
+    
+    
+    try {
+      
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    
+    
+  }
 }
